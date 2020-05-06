dbe7d019292779e83c5a0042b1a8e6752e70113c
Igor Anany <igor@7glyphs.com>
2020-05-07 10:24:50 +1200

    Fixed Xcode issue for UIKit import

--- ZoomImageView/ZoomImageView.swift
+++ ZoomImageView/ZoomImageView.swift
@@ -20,7 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#if canImport(UIKit)
+#if os(iOS)
+import UIKit
 
 open class ZoomImageView : UIScrollView, UIScrollViewDelegate {
 
